7b196d1f0b204a8d891bd0382726e7aecd1dc0ac
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:43:36 -0400

    Add files via upload

---
+++ operadoresnumericos9.py
@@ -0,0 +1,22 @@
+# Proposito: Trabajar con los operadores aritmrtico
+input('Suponga que a=49 b=5 c=3, pulse cualquier tecla para realizar el siguiente programa: ')
+input('(7 + 2)//(c - 1), pulse cualquier tecla para obtener su resultado')
+# Entrada de datos
+
+a = 49
+
+b = 5
+
+c = 3
+
+# Procedimiento
+
+z = (7 + 2) // (c - 1)
+ 
+# Salida del programa
+
+print(' El resultado del programa es: ',z)
+
+# Fin del programa
+
+input(' pulse cualquier tecla para continuar...')# pausa
